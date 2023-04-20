60b761135a3f3e2ebc45d226328db4f92f080f37
nordinsimon <112483351+nordinsimon@users.noreply.github.com>
2023-04-20 13:28:03 +0200

    Create User Login and JWT is working

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
 import express from "express";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import * as dotenv from "dotenv";
-dotenv.config();
-
 import { dbConnect } from "../db/dbConnect.js";
-import User from "../db/userModel.js";
+
+import userRoutes from "./routes/user.js";
 
 dbConnect();
 
@@ -22,74 +18,6 @@ app.use(express.json());
 app.use(logger);
 
 // Routes
-
-app.post("/register", (req, res) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hashedPassword) => {
-      const user = new User({
-        email: req.body.email,
-        password: hashedPassword,
-      });
-      user
-        .save()
-        .then((result) => {
-          res.status(201).send({
-            message: "User Created Successfully",
-            result,
-          });
-        })
-        .catch((error) => {
-          res.status(500).send({
-            message: "Error creating user",
-            error,
-          });
-        });
-    })
-    .catch((e) => {
-      res.status(500).send({
-        message: "Password was not hashed successfully",
-        e,
-      });
-    });
-});
-
-app.post("/login", (req, res) => {
-  User.findOne({ email: req.body.email })
-
-    .then((user) => {
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then((passwordCheck) => {
-          if (!passwordCheck) {
-            return res.status(401).send({
-              message: "Password is incorrect",
-            });
-          }
-          const token = jwt.sign(
-            { id: user._id, email: user.email },
-            process.env.JWT_SECRET,
-            { expiresIn: "1h" }
-          );
-          res.status(200).send({
-            message: "Login successful",
-            email: user.email,
-            token,
-          });
-        })
-        .catch((e) => {
-          res.status(401).send({
-            message: "Password is incorrect",
-            e,
-          });
-        });
-    })
-    .catch((e) => {
-      res.status(404).send({
-        message: "User not found",
-        e,
-      });
-    });
-});
+app.use("/user", userRoutes);
 
 export { app };
---
+++ src/routes/user.js
@@ -0,0 +1,97 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
+
+import User from "../../db/userModel.js";
+
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: req.body.email,
+        password: hashedPassword,
+      });
+      user
+        .save()
+        .then((result) => {
+          res.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        .catch((error) => {
+          res.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    .catch((e) => {
+      res.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
+});
+
+router.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email })
+
+    .then((user) => {
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((passwordCheck) => {
+          if (!passwordCheck) {
+            return res.status(401).send({
+              message: "Password is incorrect",
+            });
+          }
+          const token = jwt.sign(
+            { id: user._id, email: user.email },
+            JWT_SECRET,
+            { expiresIn: JWT_EXPIRES_IN }
+          );
+          res.status(200).send({
+            message: "Login successful",
+            email: user.email,
+            token,
+          });
+        })
+        .catch((e) => {
+          res.status(401).send({
+            message: "Password is incorrect",
+            e,
+          });
+        });
+    })
+    .catch((e) => {
+      res.status(404).send({
+        message: "User not found",
+        e,
+      });
+    });
+});
+router.get("/JWT", (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: "Unauthorized",
+      });
+    }
+    res.status(200).send({
+      message: "Authorized",
+      decoded,
+    });
+  });
+});
+
+export default router;
