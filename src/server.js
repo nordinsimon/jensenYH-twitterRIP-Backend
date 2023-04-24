c5b18c7028f394efe54efe29839a8919987c0833
Oscar Wendt <oscar.wendt91@gmail.com>
2023-04-24 23:10:18 +0200

    added cors changes to server and dependencies

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import { app } from "./app.js";
 import * as dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log("Server is listening on port " + PORT);
