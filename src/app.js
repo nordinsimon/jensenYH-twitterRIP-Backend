80e4ccc7ac29e0977bc79299f884b0dbce963b31
Oscar Wendt <oscar.wendt91@gmail.com>
2023-04-26 00:42:30 +0200

    added cors to app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { dbConnect } from "../db/dbConnect.js";
 
 import userRoutes from "./routes/user.js";
@@ -14,8 +15,15 @@ const logger = (req, res, next) => {
   next();
 };
 
+app.use(cors());
 app.use(express.json());
 app.use(logger);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 // Routes
 app.use("/user", userRoutes);
